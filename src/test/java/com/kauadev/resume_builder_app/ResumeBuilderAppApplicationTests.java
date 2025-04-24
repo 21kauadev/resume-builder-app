130f5ea7d1324ef9a5d59798bb05a51884a2d5b4
21kauadev <kauantnetsoares@gmail.com>
2025-04-24 11:09:43 -0300

    initial commit

---
+++ src/test/java/com/kauadev/resume_builder_app/ResumeBuilderAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kauadev.resume_builder_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeBuilderAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
