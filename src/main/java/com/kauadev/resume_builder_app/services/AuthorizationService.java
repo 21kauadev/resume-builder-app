19849d6136b31bb3172772e9901b26717ff4465b
21kauadev <kauantnetsoares@gmail.com>
2025-04-28 11:54:55 -0300

    feat: add method findByUsername to userRepository and add AuthorizationService

---
+++ src/main/java/com/kauadev/resume_builder_app/services/AuthorizationService.java
@@ -0,0 +1,19 @@
+package com.kauadev.resume_builder_app.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.kauadev.resume_builder_app.repositories.UserRepository;
+
+public class AuthorizationService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return this.userRepository.findByUsername(username);
+    }
+}
