6a6557f7b2a8bf922ccc8f8ce0eac4d6f4f1c2d5
21kauadev <kauantnetsoares@gmail.com>
2025-05-02 10:52:47 -0300

    feat: add JwtAuthFilter with recoverToken method to get the token

---
+++ src/main/java/com/kauadev/resume_builder_app/infra/security/JwtAuthFilter.java
@@ -0,0 +1,48 @@
+package com.kauadev.resume_builder_app.infra.security;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.kauadev.resume_builder_app.repositories.UserRepository;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private TokenService tokenService;
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        try {
+
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
+    }
+
+    protected String recoverToken(HttpServletRequest request) {
+        String authHeader = request.getHeader("AUTHORIZATION");
+
+        if (authHeader == null) {
+            return null;
+        }
+
+        // onde tiver Bearer, troca por espaço vazio, restando só o token.
+        String token = authHeader.replace("Bearer", "");
+
+        return token;
+    }
+
+}
