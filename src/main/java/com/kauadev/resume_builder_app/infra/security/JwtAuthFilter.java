70133a86124fb3585c0794005ea38484a1f04c91
21kauadev <kauantnetsoares@gmail.com>
2025-05-02 10:56:52 -0300

    feat: finish JwtAuthFilter with doFilterInternal method implementation

--- src/main/java/com/kauadev/resume_builder_app/infra/security/JwtAuthFilter.java
+++ src/main/java/com/kauadev/resume_builder_app/infra/security/JwtAuthFilter.java
@@ -3,6 +3,10 @@ package com.kauadev.resume_builder_app.infra.security;
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -25,7 +29,20 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
         try {
+            String token = this.recoverToken(request);
 
+            if (token != null) {
+                String usernameSubject = tokenService.validateTokenAndReturnSubject(token);
+
+                UserDetails user = userRepository.findByUsername(usernameSubject);
+
+                Authentication authentication = new UsernamePasswordAuthenticationToken(user, null,
+                        user.getAuthorities());
+
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+
+            filterChain.doFilter(request, response);
         } catch (Exception e) {
             // TODO: handle exception
         }
