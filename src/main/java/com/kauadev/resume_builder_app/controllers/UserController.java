35e6ab6e500d1bb547a37917494e4309a59afe68
21kauadev <kauantnetsoares@gmail.com>
2025-05-03 12:02:17 -0300

    feat: add updateUser and deleteUser routes in UserController and returns response with ApiResponse class

--- src/main/java/com/kauadev/resume_builder_app/controllers/UserController.java
+++ src/main/java/com/kauadev/resume_builder_app/controllers/UserController.java
@@ -5,12 +5,16 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kauadev.resume_builder_app.domain.user.User;
+import com.kauadev.resume_builder_app.domain.user.UserDTO;
 import com.kauadev.resume_builder_app.infra.ApiResponse;
 import com.kauadev.resume_builder_app.services.UserService;
 
@@ -24,7 +28,6 @@ public class UserController {
     @GetMapping("/")
     private ResponseEntity<ApiResponse<List<User>>> getAllUsers() {
         List<User> users = userService.getAllUsers();
-
         ApiResponse<List<User>> response = new ApiResponse<List<User>>(
                 HttpStatus.OK.value(), true,
                 "Estes foram os usuários encontrados: ",
@@ -39,6 +42,23 @@ public class UserController {
         ApiResponse<User> response = new ApiResponse<User>(HttpStatus.OK.value(), true, "Usuário encontrado.", user);
 
         return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @PutMapping("/{id}")
+    private ResponseEntity<ApiResponse<User>> updateUser(@PathVariable("id") Long id, @RequestBody UserDTO data) {
+        User user = userService.updateUser(id, data);
+        ApiResponse<User> response = new ApiResponse<>(HttpStatus.OK.value(), true, "Dados do usuário atualizados.",
+                user);
+
+        return ResponseEntity.ok().body(response);
+    }
+
+    @DeleteMapping("/{id}")
+    private ResponseEntity<ApiResponse<Void>> deleteUser(@PathVariable("id") Long id) {
+        userService.deleteUser(id);
+        ApiResponse<Void> response = new ApiResponse<>(HttpStatus.OK.value(), true, "Usuário deletado", null);
+
+        return ResponseEntity.ok().body(response);
     }
 
 }
